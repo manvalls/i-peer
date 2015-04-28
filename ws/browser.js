c81f7f0308bece6a0d72bf33a48028b017e3ecb1
Manuel Valls Fernández <manolo@vlrz.es>
2015-04-28 19:34:20 +0200

    Added from/ws

--- ws/browser.js
+++ ws/browser.js
@@ -1,56 +1,12 @@
-var Emitter = require('y-emitter'),
-    
-    on = require('u-proto/on'),
-    once = require('u-proto/once');
+var from = require('../from/ws.js');
 
 module.exports = function getPeer(url){
-  var inP,outP,ws;
   
   if(!url.match(/^wss?:/)){
     if(url.charAt(0) == '/') url = location.origin.replace(/^http/,'ws') + url + '/';
     else url = document.baseURI.replace(/^http/,'ws') + url + '/';
   }
   
-  ws = new WebSocket(url);
-  
-  inP = new Emitter.Hybrid();
-  outP = new Emitter.Hybrid();
-  Emitter.chain(inP,outP);
-  
-  ws[once]('open',onceOpen,inP);
-  ws[once]('close',onceClosed,inP);
-  ws[on]('message',onMsg,inP);
-  
-  inP.on('msg',sendMsg,ws);
-  inP.once('closed',close,ws);
-  
-  return outP;
-}
-
-function onceOpen(e,en,inP){
-  inP.set('ready');
-}
-
-function onceClosed(e,en,inP){
-  inP.unset('ready');
-  inP.set('closed');
-}
-
-function onMsg(e,en,inP){
-  var msg = e.data;
-  
-  msg = JSON.parse(msg);
-  inP.give('msg',msg);
-}
-
-function sendMsg(msg,en,ws){
-  ws.send(JSON.stringify(msg));
-}
-
-function close(e,en,ws){
-  ws.close();
-  
-  this.unset('ready');
-  this.set('closed');
+  return from(new WebSocket(url));
 }
 
--- ws/node.js
+++ ws/node.js
@@ -1,51 +1,7 @@
-var Emitter = require('y-emitter'),
-    WebSocket = require('ws'),
-    
-    on = require('u-proto/on'),
-    once = require('u-proto/once');
+var WebSocket = require('ws'),
+    from = require('../from/ws.js');
 
 module.exports = function getPeer(url){
-  var inP,outP,
-      ws = new WebSocket(url);
-  
-  inP = new Emitter.Hybrid();
-  outP = new Emitter.Hybrid();
-  Emitter.chain(inP,outP);
-  
-  ws[once]('open',onceOpen,inP);
-  ws[once]('close',onceClosed,inP);
-  ws[on]('message',onMsg,inP);
-  
-  inP.on('msg',sendMsg,ws);
-  inP.once('closed',close,ws);
-  
-  return outP;
-}
-
-function onceOpen(e,en,inP){
-  inP.set('ready');
-}
-
-function onceClosed(e,en,inP){
-  inP.unset('ready');
-  inP.set('closed');
-}
-
-function onMsg(e,en,inP){
-  var msg = e[0];
-  
-  msg = JSON.parse(msg);
-  inP.give('msg',msg);
-}
-
-function sendMsg(msg,en,ws){
-  ws.send(JSON.stringify(msg));
-}
-
-function close(e,en,ws){
-  ws.close();
-  
-  this.unset('ready');
-  this.set('closed');
-}
+  return from(new WebSocket(url));
+};
 
